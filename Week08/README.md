7449f44b784a2ad7f940afafe7658652674fab05
maJun <1422205209@qq.com>
2021-01-24 12:13:02 +0800

    八:添加node的service

--- Week08/README.md
+++ Week08/README.md
@@ -1,1 +1,16 @@
-学习笔记
+# 学习笔记
+
+# 有限状态机
+
+# 
+## HTTP 请求
+
+POST /HTTP/1.1
+Host:127.0.0.1
+Content-Type: application/x-www-form-urlencoded
+
+
+
+Request line：method / path / HTTP和版本
+headers：多行，以空白行结束
+body：格式和结构，根据 Content-Type 的不同而不同
---
+++ Week08/nodeService.js
@@ -0,0 +1,20 @@
+const http = require('http') // 基础的 http 服务
+
+let server = function (request, response) {
+  // response.setHeader('Access-Control-Allow-Origin', '*') // 允许跨域的请求头
+  let body = [] // {string} 接收的数据 
+  // 监听'data'事件,收data的传参
+  request.on('error', error => {
+    console.error(error)
+  }).on('data', data => {
+    body.push(data)
+  }).on('end', () => { // 监听'end'事件,接参结束
+    // 用 querystring 格式化接收到的参数
+    body = Buffer.concat(body).toString() // {object} 接收的数据
+    console.log(body)
+    response.writeHead(200, {'Content-Type': 'text/html'}) // 把数据返回给前台
+    response.end('Hello world\n')
+  })
+}
+
+http.createServer(server).listen(1111)
