2c702de82c58082519926eba6e71cbc0d9f2c70f
maJun <1422205209@qq.com>
2021-03-12 09:21:11 +0800

    十四:轮播图一,展示所有图片功能

--- Week14/main.js
+++ Week14/main.js
@@ -1,80 +1,37 @@
-function createElement(type, attributes, ...children) {
-  let element;
-
-  if (typeof type === 'string') {
-    element = new ElementWrapper(type)
-  } else {
-    element = new type()
-  }
-
-  if (attributes) {
-    for (let name in attributes) {
-      element.setAttribute(name, attributes[name])
-    }
-  }
-
-  for (child of children) {
-    if (typeof child === 'string') {
-      child = new TextWrapper(child)
-    }
-    child.mountTo(element)
-  }
-
-  return element
-}
-
-class ElementWrapper {
-  constructor(type) {
-    this.root = document.createElement(type)
-  }
-
-  setAttribute(name, value) {
-    this.root.setAttribute(name, value)
-  }
-
-  appendChild(child) {
-    this.root.appendChild(child)
-  }
-
-  mountTo(parent) {
-    parent.appendChild(this.root)
-  }
-}
-
-class TextWrapper {
-  constructor(text) {
-    this.root = document.createTextNode(text)
-  }
-
-  mountTo(parent) {
-    parent.appendChild(this.root)
-  }
-}
-
-class Div {
+import { Component, createElement } from './framework'
+class Carousel extends Component {
   constructor() {
-    this.root = document.createElement('div')
+    super()
+    this.attributes = Object.create(null)
   }
-
   setAttribute(name, value) {
-    this.root.setAttribute(name, value)
+    this.attributes[name] = value
   }
-
-  appendChild(child) {
-    this.root.appendChild(child)
+  render() {
+    this.root = document.createElement('div')
+    let imgs = this.attributes.src
+    for (let src of imgs) {
+      let imgDom = document.createElement('img')
+      imgDom.src = src
+      this.root.appendChild(imgDom)
+    }
+    return this.root
   }
-
   mountTo(parent) {
-    parent.appendChild(this.root)
+    parent.appendChild(this.render())
   }
-
 }
 
+let imgs = [
+  'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
+  'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
+  'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
+  'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg',
+]
 let a = (
-  <Div id="a">
-    <span>1</span>
-    <span>2</span>
-  </Div>
+  <Carousel src={imgs}></Carousel>
 )
+
+
 
 a.mountTo(document.querySelector('body'))
