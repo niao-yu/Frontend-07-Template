7c9a69c4a6a550173109d98af9ef11120b9a23c9
maJun <1422205209@qq.com>
2021-03-12 08:52:21 +0800

    十四:基本功能原理

--- Week14/main.js
+++ Week14/main.js
@@ -1,4 +1,80 @@
-for (let i of [1, 3, 5]) {
-  console.log(i)
-  console.log(<div>标签</div>)
+function createElement(type, attributes, ...children) {
+  let element;
+
+  if (typeof type === 'string') {
+    element = new ElementWrapper(type)
+  } else {
+    element = new type()
+  }
+
+  if (attributes) {
+    for (let name in attributes) {
+      element.setAttribute(name, attributes[name])
+    }
+  }
+
+  for (child of children) {
+    if (typeof child === 'string') {
+      child = new TextWrapper(child)
+    }
+    child.mountTo(element)
+  }
+
+  return element
 }
+
+class ElementWrapper {
+  constructor(type) {
+    this.root = document.createElement(type)
+  }
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value)
+  }
+
+  appendChild(child) {
+    this.root.appendChild(child)
+  }
+
+  mountTo(parent) {
+    parent.appendChild(this.root)
+  }
+}
+
+class TextWrapper {
+  constructor(text) {
+    this.root = document.createTextNode(text)
+  }
+
+  mountTo(parent) {
+    parent.appendChild(this.root)
+  }
+}
+
+class Div {
+  constructor() {
+    this.root = document.createElement('div')
+  }
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value)
+  }
+
+  appendChild(child) {
+    this.root.appendChild(child)
+  }
+
+  mountTo(parent) {
+    parent.appendChild(this.root)
+  }
+
+}
+
+let a = (
+  <Div id="a">
+    <span>1</span>
+    <span>2</span>
+  </Div>
+)
+
+a.mountTo(document.querySelector('body'))
